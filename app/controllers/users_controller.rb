1233e32bf45365ce18bc030f0f6962651851a77b
Jordan Morano <jordan.morano@gmail.com>
2015-04-01 10:17:24 -0400

    Update create action for user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  skip_before_filter :set_current_user
+
   def create
-    render json: {}, status: :created
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 end
